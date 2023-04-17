dd84533de477a690d9a6f6e6b5a7ee6aa95b3c83
Peter Harbst <pharbst@2-D-4.42heilbronn.de>
2023-04-17 14:39:52 +0200

    ex02 done

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 14:30:39 by pharbst           #+#    #+#             */
+/*   Updated: 2023/04/17 14:38:42 by pharbst          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main()
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string* stringPTR = &string;
+	std::string& stringREF = string;
+
+	std::cout << "address of string:         " << &string << std::endl;
+	std::cout << "address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "address of stringREF:      " << &stringREF << std::endl;
+	std::cout << "value of string:           " << string << std::endl;
+	std::cout << "value of stringPTR:        " << *stringPTR << std::endl;
+	std::cout << "value of stringREF:        " << stringREF << std::endl;
+	return (0);
+}
