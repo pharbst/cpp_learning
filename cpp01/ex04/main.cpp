3d97f9fb18e9103294ecd3fa4a95e069e7fa548b
Peter Harbst <pharbst@2-H-4.42heilbronn.de>
2023-04-18 22:03:50 +0200

    WIP:	ex04 fixed class constructer

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 15:12:26 by pharbst           #+#    #+#             */
-/*   Updated: 2023/04/17 15:55:27 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/04/18 18:22:27 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,17 @@
 
 class	FileEditor
 {
-	FileEditor(const std::string& filename, const std::string& from, const std::string& to) : _filename(filename), _from(from), _to(to), _fileOutName(filename.append(.replace))
-	{}
+	public:
+		FileEditor(const std::string& filename, const std::string& from, const std::string& to) : _filename(filename), _from(from), _to(to), _fileOutName(filename + ".replace"){}
+		~FileEditor(void){}
+		void		Replace();
 
-	~FileEditor(void)
-	{}
-	
-	void	Replace()
-}
+	private:
+		std::string	_filename;
+		std::string	_from;
+		std::string	_to;
+		std::string	_fileOutName;
+};
 
 int	main(int argc, char *argv[])
 {
@@ -39,16 +42,15 @@ int	main(int argc, char *argv[])
 		std::cout << std::endl;
 		
 	}
-	if (argc != 4)
+	else if (argc != 4)
 	{
 		std::cerr << "Bad usage" << std::endl;
-		std::cout << "Usage: ./sedForLoosers <filename> <string to replace> <string to replace with>" << std::endl;
+		std::cout << "Usage: ./sedForLoosers <filename> <string to replace> <string to replace with>" << std::endl << "or just: ./sedForLoosers" << std::endl;
 		return (1);
 	}
 
 	std::string	filename = argv[1];
 	std::string	s1 = argv[2];
 	std::string	s2 = argv[3];
-
 	
 }
