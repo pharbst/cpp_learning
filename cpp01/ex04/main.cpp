467651bf7ae7554edd22ed7965cfe2b46dcf65e8
pharbst <pharbst@student.42heilbronn.de>
2023-04-19 09:58:45 +0200

    Progress:	ex04 done and a simple testfile is provided

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 15:12:26 by pharbst           #+#    #+#             */
-/*   Updated: 2023/04/18 18:22:27 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/04/19 09:53:11 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,44 @@ class	FileEditor
 	public:
 		FileEditor(const std::string& filename, const std::string& from, const std::string& to) : _filename(filename), _from(from), _to(to), _fileOutName(filename + ".replace"){}
 		~FileEditor(void){}
-		void		Replace();
+		
+		void	modifyLine(std::string& line)
+		{
+			std::string	result;
+			size_t		start = 0;
+			size_t		end = line.find(_from);
+
+			while (end != std::string::npos)
+			{
+				result += line.substr(start, end - start) + _to;
+				start = end + _from.length();
+				end = line.find(_from, start);
+			}
+			result += line.substr(start);
+			line = result;
+    	}
+
+		void		Replace()
+		{
+			std::ifstream	fileIn(_filename);
+			if (!fileIn.is_open())
+			{
+				std::cerr << "Error: could not open file" << _filename << std::endl;
+				return ;
+			}
+			std::ofstream	fileOut(_fileOutName);
+			if (!fileOut.is_open())
+			{
+				std::cerr << "Error: could not open file" << _fileOutName << std::endl;
+				return ;
+			}
+			std::string		line;
+			while (std::getline(fileIn, line))
+			{
+				modifyLine(line);
+				fileOut << line << std::endl;
+			}
+		}
 
 	private:
 		std::string	_filename;
@@ -30,6 +67,10 @@ class	FileEditor
 
 int	main(int argc, char *argv[])
 {
+	std::string	filename;
+	std::string	from;
+	std::string	to;
+
 	if (argc == 1)
 	{
 		std::string	filename, from, to;
@@ -48,9 +89,13 @@ int	main(int argc, char *argv[])
 		std::cout << "Usage: ./sedForLoosers <filename> <string to replace> <string to replace with>" << std::endl << "or just: ./sedForLoosers" << std::endl;
 		return (1);
 	}
-
-	std::string	filename = argv[1];
-	std::string	s1 = argv[2];
-	std::string	s2 = argv[3];
-	
+	else if (argc == 4)
+	{
+		filename = argv[1];
+		from = argv[2];
+		to = argv[3];
+	}
+	FileEditor	fileEditor(filename, from, to);
+	fileEditor.Replace();
+	return (0);
 }
