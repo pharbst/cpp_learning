0589ca7ef9caf1c69fda146cb0b8e60d3e647be7
Peter Harbst <pharbst@2-G-8.42heilbronn.de>
2023-05-17 17:36:00 +0200

    added a normal animal to the main function

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 12:51:53 by pharbst           #+#    #+#             */
-/*   Updated: 2023/05/17 17:32:34 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/05/17 17:35:02 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@
 int	main(){
 	Animal*	puppy = new Dog();
 	Animal*	kitty = new Cat();
+	Animal* animal = new Animal();
 	
 	puppy->makeSound();
 	kitty->makeSound();
+	animal->makeSound();
 	delete puppy;
 	delete kitty;
+	delete animal;
 }
