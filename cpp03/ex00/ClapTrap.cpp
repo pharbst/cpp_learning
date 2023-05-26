6f230c2b40f52278da3ebb6af94e3534b69d0b7b
Peter Harbst <pharbst@2-G-8.42heilbronn.de>
2023-05-26 11:34:01 +0200

    Fix:	little fix in ex00 of cpp03

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 16:02:05 by pharbst           #+#    #+#             */
-/*   Updated: 2023/05/24 14:33:08 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/05/26 11:33:17 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,10 @@ ClapTrap::ClapTrap(ClapTrap const & ref){
 
 ClapTrap& ClapTrap::operator = (ClapTrap const & ref){
 	std::cout << "\033[0;32mCopy assignment operator called\033[0m" << std::endl;
-	this->_name = ref._name;
-	this->_hitpoints = ref._hitpoints;
-	this->_energyPoints = ref._energyPoints;
-	this->_attackDamage = ref._attackDamage;
+	_name = ref._name;
+	_hitpoints = ref._hitpoints;
+	_energyPoints = ref._energyPoints;
+	_attackDamage = ref._attackDamage;
 	return (*this);
 }
 
@@ -43,17 +43,17 @@ void ClapTrap::attack(std::string const & target){
 		std::cout << "\033[0;31mClapTrap " << _name << " can't attack, it's out of energy!\033[0m" << std::endl;
 		return ;
 	}
-	std::cout << "\033[0;33mClapTrap " << this->_name << " attacks " << target << ", causing " << this->_attackDamage << " points of damage!\033[0m" << std::endl;
+	std::cout << "\033[0;33mClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!\033[0m" << std::endl;
 	_energyPoints--;
 }
 
 void ClapTrap::takeDamage(unsigned int amount){
 	if (_hitpoints - amount <= 0){
+		std::cout << "\033[0;31mClapTrap " << _name << " takes " << _hitpoints << " points of damage and dies!\033[0m" << std::endl;
 		_hitpoints = 0;
-		std::cout << "\033[0;31mClapTrap " << _name << " is dead!\033[0m" << std::endl;
 		return ;
 	}
-	std::cout << "\033[0;33mClapTrap " << this->_name << " takes " << amount << " points of damage!\033[0m" << std::endl;
+	std::cout << "\033[0;33mClapTrap " << _name << " takes " << amount << " points of damage!\033[0m" << std::endl;
 	_hitpoints -= amount;
 }
 
@@ -63,7 +63,7 @@ void ClapTrap::beRepaired(unsigned int amount){
 	else if (_energyPoints <= 0)
 		std::cout << "\033[0;31mClapTrap " << _name << " can't be repaired, it's out of energy!\033[0m" << std::endl;
 	else{
-		std::cout << "\033[0;33mClapTrap " << this->_name << " is repaired for " << amount << " points of damage!\033[0m" << std::endl;
+		std::cout << "\033[0;33mClapTrap " << _name << " is repaired for " << amount << " points of damage!\033[0m" << std::endl;
 		_energyPoints--;
 		_hitpoints += amount;
 	}
