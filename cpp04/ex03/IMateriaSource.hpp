04019cffd7d1e4a28eec0f133db26e52bee5599d
pharbst <pharbst@student.42heilbronn.de>
2023-05-29 11:28:15 +0200

    ToDo:	add implementaition of IMateriaSource 	fix return values of equip and unequip

---
+++ cpp04/ex03/IMateriaSource.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IMateriaSource.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/29 11:01:42 by pharbst           #+#    #+#             */
+/*   Updated: 2023/05/29 11:03:31 by pharbst          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IMATERIASOURCE_HPP
+# define IMATERIASOURCE_HPP
+
+#include <iostream>
+class AMateria;
+
+class IMateriaSource
+{
+    public:
+        virtual ~IMateriaSource() {}
+        virtual void learnMateria(AMateria*) = 0;
+        virtual AMateria* createMateria(std::string const & type) = 0;
+};
+
+#endif
