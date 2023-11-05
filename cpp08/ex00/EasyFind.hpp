584180044c480e739e6b5b1be6e5efda7bc0ec2f
Peter Harbst <pharbst@2-C-4.42heilbronn.de>
2023-11-06 00:41:56 +0100

    Fix:	stupid gitconfig ignored file renaming

---
+++ cpp08/ex00/EasyFind.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/23 17:48:07 by pharbst           #+#    #+#             */
+/*   Updated: 2023/09/24 14:05:51 by pharbst          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <algorithm>
+# include <vector>
+# include <list>
+
+
+class TargetNotFoundException : public std::exception {
+public:
+	virtual const char* what() const throw();
+};
+
+template <typename T, typename S>
+unsigned int	easyfind(T container, S target) {
+	typename T::iterator it = std::find(container.begin(), container.end(), target);
+
+	if (it != container.end())
+		return std::distance(container.begin(), it);
+	throw TargetNotFoundException();
+}
+
+#endif
